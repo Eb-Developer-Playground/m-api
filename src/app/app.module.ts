6a3676053a851fa4d7fafe4582bebbe7bd0f7427
Owen Adirah <owenadira@gmail.com>
2024-01-08 14:44:26 +0300

    declare generated data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {UserProfileComponent} from './user/user-profile/user-profile.component';
+import {ApiListComponent} from './api-list/api-list.component';
+import {UserUpdateDetailsComponent} from './user/user-profile/user-update-details/user-update-details.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {ApiDetailsComponent} from './api-list/api-details/api-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UserProfileComponent,
+    ApiListComponent,
+    UserUpdateDetailsComponent,
+    PageNotFoundComponent,
+    ApiDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +29,5 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
