591f3cbb134908a204bcd91fc38cfe24528aa4fc
Owen Adirah <owenadira@gmail.com>
2024-01-08 14:44:54 +0300

    define the routing tree

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,49 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {UserProfileComponent} from "./user/user-profile/user-profile.component";
+import {ApiListComponent} from "./api-list/api-list.component";
+import {UserUpdateDetailsComponent} from "./user/user-profile/user-update-details/user-update-details.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {ApiDetailsComponent} from "./api-list/api-details/api-details.component";
+import {authGuard} from "./auth/guard/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'users',
+    component: UserProfileComponent
+  },
+  {
+    path: 'user/:id',
+    component: UserUpdateDetailsComponent
+  },
+  {
+    path: 'apis',
+    component: ApiListComponent
+  },
+  {
+    path: 'apis/:id',
+    component: ApiDetailsComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
