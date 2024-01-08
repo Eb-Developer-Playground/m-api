c73995106511a7ca283e04976eeddf1cf4d4a6d9
Owen Adirah <owenadira@gmail.com>
2024-01-08 14:41:45 +0300

    component for user feature

---
+++ src/app/user/user-profile/user-update-details/user-update-details.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-user-update-details',
+  templateUrl: './user-update-details.component.html',
+  styleUrls: ['./user-update-details.component.scss']
+})
+export class UserUpdateDetailsComponent {
+
+}
