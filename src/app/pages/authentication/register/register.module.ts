a1d214d77a9458f76b97043e699ad3c3185d036d
Owen Adirah <owenadira@gmail.com>
2024-01-10 00:36:11 +0300

    user login feature

---
+++ src/app/pages/authentication/register/register.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterRoutingModule } from './register-routing.module';
+import { RegisterComponent } from './register.component';
+import { MaterialModule } from '../../../../@equity-bank/shared/material-components.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RegisterRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ],
+  declarations: [RegisterComponent]
+})
+export class RegisterModule {
+}
